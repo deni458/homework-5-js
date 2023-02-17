50990808017231ac7b0365690c900d01971137ef
Dejan Stamenov <122811403+deni458@users.noreply.github.com>
2023-02-17 14:14:31 +0100

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,12 +1,9 @@
 let names = ['Jocka', 'Mark', 'Nikola', 'Test', 'Test12', 'Lazar', 'Jovan'];
 
-let evenNames = 0;
-
 let i = 0;
 while (i < names.length) {
     if (names[i].length % 2 === 0) {
-        evenNames = names[i];
-        console.log(evenNames);
+        console.log(names[i]);
     }
     i++;
 }
